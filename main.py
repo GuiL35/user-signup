4efaa13f8b526457d17af1aa8711192e8665b375
LG <yolandagui9@gmail.com>
2017-06-21 20:42:50 -0700

    fix some bugs in user-signup

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 from flask import Flask,request
+import cgi
+import os 
+import jinja2
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-
-<html>
-    <title>"Signup"</title>
-    <body>
-        <form action = "/signup", method = "POST">
-            <label for = "username">Username<label>
-            <input type = "text", id = "username" />
-
-            <label for = "password">Password<label>
-            <input type = "text", id = "password" />  
-
-            <label for = "verification">Verfiy Password<label>
-            <input type = "text", id = "verifcation" />  
-                
-            <label for = "email">Email(optional)<label>
-            <input type = "text", id = "email" />
-
-            <input type = "submit", id = "button" />
-        </form>
-    </body>
-</html> """
 
 @app.route("/")
 def index():
-    return form
+    template = jinja_env.get_template('signup.html')
+    return template.render()
 
-@app.route("/signup", methods = ["POST"])
+
+@app.route("/signup", methods = ['POST'])
 def user_signup():
+    saved_user_info = ["username","email"]
     signup_name = request.form['username']
-    return "welcome " + signup_name
+    password = request.form['password']
+    verification = request.form['verification']
+    template = jinja_env.get_template('welcome_greeting.html')
+    saved_username_email= request.args.get('saved_user_info')
+    return template.render(username = signup_name, password=password,verification=verification, saved_username_email= saved_username_email )
+
+app.run()
